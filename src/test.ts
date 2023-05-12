22914658ed056205453c23a11e7c8bfa0fc735d1
Philipp Koch <me@philipps.blog>
2023-05-12 14:48:40 +0200

    Moving interface generation to proper function

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,62 @@
 import * as ts from "typescript";
-import {EmitHint, ParameterDeclaration, PropertyDeclaration, SignatureKind, TypeNode} from "typescript";
+import {
+    ClassDeclaration,
+    EmitHint,
+    InterfaceDeclaration,
+    ParameterDeclaration, Program,
+    PropertyDeclaration,
+    SignatureKind,
+    TypeNode
+} from "typescript";
 
 const rpcServiceInterfaceName = 'RpcService';
 
+function classForRpcServiceInterface(itf: InterfaceDeclaration, program: Program): ClassDeclaration {
+    const properties: PropertyDeclaration[] = [];
+    const interfaceType = program.getTypeChecker().getTypeAtLocation(itf);
+
+    interfaceType.getProperties().forEach(property => {
+        const propertyType = program.getTypeChecker().getTypeOfSymbolAtLocation(property, itf);
+        const signature = program.getTypeChecker().getSignaturesOfType(propertyType, SignatureKind.Call)[0];
+
+        const typeParameters: ParameterDeclaration[] = signature.getParameters().map(param => {
+            return ts.factory.createParameterDeclaration(
+                undefined,
+                undefined,
+                param.name,
+                undefined,
+                program.getTypeChecker().typeToTypeNode(program.getTypeChecker().getTypeOfSymbol(param), undefined, undefined),
+            )
+        });
+        const returnType: TypeNode = ts.factory.createTypeReferenceNode('Promise', [program.getTypeChecker().typeToTypeNode(signature.getReturnType(), undefined, undefined)!])
+        const functionType = ts.factory.createFunctionTypeNode(undefined, typeParameters, returnType);
+
+
+        properties.push(ts.factory.createPropertyDeclaration(
+            ts.factory.createNodeArray([ts.factory.createToken(ts.SyntaxKind.PublicKeyword)]),
+            ts.factory.createIdentifier(property.name),
+            undefined,
+            functionType,
+            undefined
+        ));
+    });
+
+    return ts.factory.createClassDeclaration(
+        undefined,
+        ts.factory.createIdentifier(itf.name.text),
+        undefined,
+        [
+            ts.factory.createHeritageClause(ts.SyntaxKind.ImplementsKeyword,
+                [
+                    ts.factory.createExpressionWithTypeArguments(ts.factory.createIdentifier(rpcServiceInterfaceName), undefined)
+                ])
+        ],
+        properties,
+    );
+}
+
 function compile(fileNames: string[], options: ts.CompilerOptions): void {
-    let program = ts.createProgram(fileNames, options);
+    let program: Program = ts.createProgram(fileNames, options);
 
     program.getSourceFiles()
         .filter(sourceFile => !sourceFile.isDeclarationFile)
@@ -16,49 +68,9 @@ function compile(fileNames: string[], options: ts.CompilerOptions): void {
                 if (!interfaceType.getBaseTypes()?.map(type => type.getSymbol()?.name).includes(rpcServiceInterfaceName)) {
                     return;
                 }
-
-                const properties: PropertyDeclaration[] = [];
-
-                interfaceType.getProperties().forEach(property => {
-                    const propertyType = program.getTypeChecker().getTypeOfSymbolAtLocation(property, node);
-                    const signature = program.getTypeChecker().getSignaturesOfType(propertyType, SignatureKind.Call)[0];
-
-                    const typeParameters: ParameterDeclaration[] = signature.getParameters().map(param => {
-                        return ts.factory.createParameterDeclaration(
-                            undefined,
-                            undefined,
-                            param.name,
-                            undefined,
-                            program.getTypeChecker().typeToTypeNode(program.getTypeChecker().getTypeOfSymbol(param), undefined, undefined),
-                        )
-                    });
-                    const returnType: TypeNode = ts.factory.createTypeReferenceNode('Promise', [program.getTypeChecker().typeToTypeNode(signature.getReturnType(), undefined, undefined)!])
-                    const functionType = ts.factory.createFunctionTypeNode(undefined, typeParameters, returnType);
-
-
-                    properties.push(ts.factory.createPropertyDeclaration(
-                        ts.factory.createNodeArray([ts.factory.createToken(ts.SyntaxKind.PublicKeyword)]),
-                        ts.factory.createIdentifier(property.name),
-                        undefined,
-                        functionType,
-                        undefined
-                    ));
-                });
-
-                const generatedClass = ts.factory.createClassDeclaration(
-                    undefined,
-                    ts.factory.createIdentifier(node.name.text),
-                    undefined,
-                    [
-                        ts.factory.createHeritageClause(ts.SyntaxKind.ImplementsKeyword,
-                            [
-                                ts.factory.createExpressionWithTypeArguments(ts.factory.createIdentifier(rpcServiceInterfaceName),undefined)
-                            ])
-                    ],
-                    properties,
-                );
                 const printer = ts.createPrinter({ newLine: ts.NewLineKind.CarriageReturnLineFeed });
                 let file = ts.createSourceFile("generatedSource.ts", "", ts.ScriptTarget.ES2015);
+                const generatedClass = classForRpcServiceInterface(node, program);
                 console.log('#######');
                 console.log(printer.printNode(EmitHint.Unspecified, generatedClass, file));
                 console.log('#######');
