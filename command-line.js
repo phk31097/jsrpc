19565a8bde696be244bd54f933f5b22c0ea5b378
Philipp Koch <me@philipps.blog>
2023-05-18 11:58:22 +0200

    Run on command line

---
+++ command-line.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const { RpcCodeGenerator } = require('./dist/code-generator');
+const { RpcServer } = require('./dist/rpc-server');
+
+
+const GENERATE_CLASSES = 'generate-classes';
+const START_SERVER = 'start-server';
+
+const method = process.argv[2];
+
+switch(method) {
+    case GENERATE_CLASSES:
+        console.log('Generating classes');
+        const generator = new RpcCodeGenerator({
+            baseDirectory: 'test-project/src',
+            sharedDirectory: 'shared',
+            clientDirectory: 'client',
+            serverDirectory: 'server',
+        });
+        generator.generate();
+        break;
+    case START_SERVER:
+        console.log('Starting server');
+        new RpcServer({port: 3000}).listen();
+        break;
+    default:
+        throw new Error('Command not supported: ' + method);
+}
