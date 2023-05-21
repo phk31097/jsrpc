89cf4da38739c90b0381e40340c1ebc76755f8f2
Philipp Koch <me@philipps.blog>
2023-05-21 17:35:36 +0200

    Revert "Exporting server differently"
    This reverts commit 2907ce7d32799da3203ba0e9757ad8cbc25538b4.

--- src/export/index.ts
+++ src/export/index.ts
@@ -1,5 +1,6 @@
 export * from './rpc-decorator';
 export * from './rpc-request-matcher';
+export * from './rpc-server';
 export * from './rpc-service';
 export * from './rpc-client';
 export * from './rpc-client-factory';
